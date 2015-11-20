a2f2de2096edf19d1d6275b491c33f044bbf7fcd
perrotbryan <perrot.bryan@gmail.com>
2015-11-20 12:45:00 +0100

    Added ESP pre-primitive sample

---
+++ ESPsample/ESP/ESP/MainWindow.xaml.vb
@@ -0,0 +1,25 @@
+﻿Class MainWindow
+    Public Sub Draw()
+
+        Dim txtTest As New TextBlock()
+        txtTest.FontSize = 28
+        txtTest.Text = "Hello World!"
+        txtTest.Foreground = Brushes.Red
+        Canvas.SetTop(txtTest, 100)
+        Canvas.SetLeft(txtTest, 10)
+        c.Children.Add(txtTest)
+
+        Dim test As New Rectangle
+        test.Width = 50
+        test.Height = 50
+        test.Stroke = Brushes.Blue
+        Canvas.SetTop(test, 0)
+        Canvas.SetLeft(test, c.Width)
+        c.Children.Add(test)
+
+    End Sub
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Draw()
+    End Sub
+End Class
---
+++ SandBoxCheat/ESP.vb
@@ -0,0 +1,22 @@
+﻿
+Imports System.Windows.Controls
+Imports System.Windows.Media
+
+Public Class ESP
+    Public Sub DrawRectangle(xOrigin As Integer, yOrigin As Integer, xDest As Integer, yDest As Integer)
+        Dim myCanvas As New Canvas()
+        myCanvas.Background = Brushes.Transparent
+
+        Dim txt1 As New TextBlock
+        txt1.FontSize = 14
+        txt1.Text = "Hello World!"
+        Canvas.SetLeft(txt1, 10)
+        Canvas.SetTop(txt1, 100)
+        myCanvas.Children.Add(txt1)
+    End Sub
+
+    Public Sub Main()
+        DrawRectangle(0, 0, 0, 0)
+    End Sub
+
+End Class
